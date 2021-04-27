25d4723f1c6f62b678ee55c10158a41e36c5df8a
Babakailash <nathkailash2020@gmail.com>
2021-04-28 00:51:15 +0530

    Added:Position 100 Moves

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,29 +1,66 @@
-public class SnakeLadder {
-	public static final int ladder=1;
-	public static final int snake=2;
-	public static void main(String args[]) {
+import java.util.Scanner;
+import java.util.Random;
+
+public class SnakeLadder 
+{	
+	public static final int No_Play=1;
+	public static final int Ladder=2;
+	public static final int Snake=3;
+	public static final int Win_position=100;
+	public static final int Position_check = 0;
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to SnakeAndLadder program");
+
 		int position=0;
-		System.out.println("Current Position is:"+position);
-		int die=(int)Math.floor(((Math.random()*10)%6)+1);
-		int choice=(int)Math.floor((Math.random()*10)%3);
-		System.out.println("Number on die is:"+die);
+		System.out.println("Start position of player :	"+position);
+
+		//Rolling dice to get random number
 		
-		switch(choice){
-		case ladder:
-			System.out.println("Current position is:"+position);
-			position+=die;
-			System.out.println("Player got ladder, New position is:"+position);
-			break;
+		Random random = new Random();
+	    int droll = 0;
+		while (true)
+		{
+    		droll = random.nextInt(7);
+    		if(droll !=0) break;
+		}
+		System.out.println("Number of dice : "+droll);
+
+		//options
+		while (position < Win_position){
+		Random ran = new Random();
+	    int option = 0;
+		while (true)
+		{
+    		option = ran.nextInt(4);
+    		if(option !=0) break;
+		}
+		System.out.println("====OPTIONS======\nOption 1 : No Play\nOption 2 : Ladder \nOption 3 : Snake \n");
+		System.out.println("Option : "+option);
+		
+		switch (option) {
+			case No_Play:
+				System.out.println("Player stays in the same position : "+position);
+				break;
 			
-		case snake:
-			System.out.println("Current position is:"+position);
-			position-=die;
-			System.out.println("Player got the snake, New position is:"+position);
-			break;
+			case Ladder:
+				position += droll;
+				System.out.println("Player moves ahead by : "+position);
+				break;
 			
-		default:
-			System.out.println("No Play, Stay at the same position:"+position);
-			
+			case Snake:
+                position -= droll;
+                if ((position - droll) < Position_check){
+                	position = Position_check;
+                }
+				System.out.println("Player moves behind by : "+position);
+				break;				
+
+			default: 
+				System.out.println("Enter correct value");
+				break;
+			}
 		}
 	}
 
