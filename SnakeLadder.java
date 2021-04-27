b7a917e09f5e811264460e98980f8e2f4387bf72
Babakailash <nathkailash2020@gmail.com>
2021-04-27 18:59:40 +0530

    Added:Player Position

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+public class SnakeLadder
+{
+public static void main(String[] args)
+{
+   int[] player_position=new int[100];
+       player_position[0] =0;
+       int card_board=player_position[0];
+System.out.println("player_position[0] "+card_board);
+}
+}
