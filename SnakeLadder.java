68e206afbee86fd25da18281e491c09373aff68f
Babakailash <nathkailash2020@gmail.com>
2021-04-27 19:06:31 +0530

    Random Die Sanke and Ladder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,10 @@
+import java.util.*;
 public class SnakeLadder
 {
 public static void main(String[] args)
 {
-   int[] player_position=new int[100];
-       player_position[0] =0;
-       int card_board=player_position[0];
-System.out.println("player_position[0] "+card_board);
+       Random ran=new Random();
+       int num=1+ran.nextInt(6);
+System.out.println(num);
 }
 }
